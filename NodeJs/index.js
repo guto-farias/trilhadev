e7b02c68a039ba8ad03f3b4618b1dc301c6f9e17
guto-farias <gutofariasdev@gmail.com>
2024-04-06 20:40:13 -0300

    update index.js

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
-
 const server = express();
 
-// 127.0.0.1
-// localhost
+
+server.get("/hello", (req, res) => {
+    const {nome,idade} = req.query;
+
+    return res.json({
+        title: "hello world",
+        message: `Ola ${nome}, de boas?`,
+        idade: idade
+    });
+});
+
+server.get("/hello/:nome", (req, res) => {
+    const { nome } = req.params;
+
+    return res.json({
+        title:  "test params",
+        message: `teste 2 pro ${nome}, de boas?`,
+    });
+});
 
 server.listen(3000);
+
+
+
+
+
+
